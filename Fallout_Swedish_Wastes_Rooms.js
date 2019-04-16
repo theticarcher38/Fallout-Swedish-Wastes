a2c1753dd6ca80522d05b90261ab2bad2c954350
huntergoodman <hunterleegoodman@gmail.com>
2019-04-15 21:49:52 -0500

    Update Fallout_Swedish_Wastes_Rooms.js

--- Fallout_Swedish_Wastes_Rooms.js
+++ Fallout_Swedish_Wastes_Rooms.js
@@ -1,41 +1,26 @@
 var rooms = {
-    "start": {
+    "Supply Closet": {
         "description": "",
         "directions": {
-            "north": "",
-            "west": ""
+            
         }
     },
-    "": {
-        "description": "",
+
+    "Hallway1": {
+        "description": "H",
         "directions": {
-            "south": "start",
-            "north": "",
-            "east": ""
+
         }
     },
-    "": {
-        "description": "",
+
+    "Bedroom1": {
+        "description": "You awaken, upon opening your eyes which seem to have been shut for ages\
+        you are met with a floor that is covered in water. You notice at the foot of your bed there\
+        is a small metal box with a baseball bat laying next to it. The only exit that can be seen is\
+        a door that leads <b>east</b>.",
         "directions": {
-            "south": ""
+        "east": "Hallway1"
         }
     },
-    "": {
-        "description": "",
-        "directions": {
-            "west": ""
-        }
-    },
-    "bridge1": {
-        "description": "",
-        "directions": {
-            "east": "",
-            "west": ""
-        }
-    },
-    "bridge2": {
-        "description": "": {
-            "east": ""
-        }
-    }
+    
 }
