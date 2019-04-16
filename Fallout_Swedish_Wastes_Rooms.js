af95bc78a8698c935dbd2c92013b215b2e1756a2
huntergoodman <hunterleegoodman@gmail.com>
2019-04-16 08:02:44 -0500

    Update Fallout_Swedish_Wastes_Rooms.js

--- Fallout_Swedish_Wastes_Rooms.js
+++ Fallout_Swedish_Wastes_Rooms.js
@@ -1,15 +1,19 @@
 var rooms = {
     "Supply Closet": {
-        "description": "",
+        "description": "The supply closet is small, and after you overlook it there really is nothing of use here. A couple spare-parts and other misc. things. You can leave the supply closet by going <b>south</b>.",
         "directions": {
-            
+            "south": "Hallway1"
         }
     },
 
     "Hallway1": {
-        "description": "H",
+        "description": "The narrow hallway leads <b>north</b> to what looks to be a supply closet maybe something\
+        in there. It also leads <b>east</b> into what you believe is another bedroom. It also leads <b>south</b>, deeper into the vault.",
         "directions": {
-
+            "west": "Bedroom1",
+            "east": "Bedroom2",
+            "south": "Hallway2",
+            "north": "Supply Closet"
         }
     },
 
@@ -19,8 +23,46 @@ var rooms = {
         is a small metal box with a baseball bat laying next to it. The only exit that can be seen is\
         a door that leads <b>east</b>.",
         "directions": {
-        "east": "Hallway1"
+            "east": "Hallway1"
         }
     },
     
-}
+    "Bedroom2": {
+        "description": "This bedroom is in fairly bad condition. The water has weakened the bed and it has collapsed. A skeleton lays slumped against the wall, a 9mm Pistol in his hand. There is a closet ",
+        "directions": {
+        "west": "Hallway1"
+        }
+    },
+
+    "Hallway2": {
+        "description": "Text Goes Here",
+        "directions": {
+            "west": "Water Treatement Plant",
+            "east": "Power Station",
+            "south": "Hallway3"
+        }
+    },
+
+    "Water Treatement Plant": {
+        "description": "Text Goes Here",
+        "directions": {
+
+        }
+    },
+
+    "Power Station": {
+        "description": "Text Goes Here",
+        "directions": {
+
+        }
+    },
+
+    "Hallway3": {
+        "description": "Text Goes Here",
+        "directions": {
+
+        }
+    }
+
+
+};
